d81e3cbd6a156f81c173fe5a7f5f1eb4195f3de6
vindhyajee5 <vindhyajee5@gmail.com>
2024-01-11 11:04:34 +0530

    first

---
+++ src/main/java/com/vindhya/SpringBootappsazureApplication.java
@@ -0,0 +1,24 @@
+package com.vindhya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringBootappsazureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootappsazureApplication.class, args);
+	}
+	
+	
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "Hello vindhya";
+		
+	}
+
+}
